5bbcfb082d2b4e17dfd067585cbbb67e1d911ca7
Aamish0 <aamishatiq@gmail.com>
2025-11-07 12:41:57 -0600

    1 Feature left
    Added 3x3 square flip if screen size is big enough
    board is centered
    grid size is controlled by global variable called gridSize

--- 21 2D Array Basics/sketch.js
+++ 21 2D Array Basics/sketch.js
@@ -11,20 +11,29 @@ let rndclr = [0, 255];
 
 let maxSquares;
 
+let flipMode = "cross"; //or square
 
-let grid = [[],[]];
+let grid = [];
+let gridSize = 8;
 
-
+let canFlipSquare = false;
 let rows;
 let cols;
 
 let squareSize = 80;
 
+let warningTimer = 0;
+
+
 function setup() {
   // createCanvas(cols*squareSize, rows*squareSize);
   createCanvas(windowWidth, windowHeight);
-  randomizeBoard()
+  randomizeBoard(gridSize)
 
+  if(gridSize >= 6){
+    canFlipSquare = true;
+  }
+  else canFlipSquare = false;
   
 }
 
@@ -32,74 +41,103 @@ function draw() {
   background(31);
   renderGrid();
   checkWin()
-  // print(getCurrentY(), getCurrentX());
+  flipWarning()
+
 }
 
-function randomizeBoard(){
-  // grid = [
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)],
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)],
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)],
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)],
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)],
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)],
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)],
-  //   [random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)]
-  // ];
+function randomizeBoard(size) {
+  grid = [];
 
-  for(let i = 0; i < 10; i++){
-    grid.push([random(rndclr),       random(rndclr),    random(rndclr),  random(rndclr), random(rndclr),    random(rndclr),  random(rndclr),       random(rndclr)]);
+  for (let i = 0; i < size; i++) {
+    let row = [];
+    for (let j = 0; j < size; j++) {
+      row.push(random(rndclr));
+    }
+    grid.push(row);
   }
 
-  rows = grid.length;
-  cols = grid[0].length;
+  rows = cols = size;
   maxSquares = rows * cols;
 }
-
 
 function renderGrid(){
   // interpret the information in  the 2D array, 
   // and draw a grid of square on the screen to reflect it.
-  for(let x = 0; x < cols; x++){
-    for(let y = 0; y < rows; y++){
-      let fillColor = grid[y][x];
-      fill(fillColor);
-      square(x*squareSize, y*squareSize, squareSize);
-      
-    }
+  let gridWidth = cols * squareSize;
+  let gridHeight = rows * squareSize;
+  let startX = (width - gridWidth) / 2;
+  let startY = (height - gridHeight) / 2;
 
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      fill(grid[y][x]);
+      square(startX + x * squareSize, startY + y * squareSize, squareSize);
+    }
   }
 }
 
-function mousePressed(){
-  //check for win
-  
+function keyPressed(){
+  if (key === ' ') {
+    if (flipMode === "cross" && canFlipSquare) {
+      flipMode = "square";
+    } else if (flipMode === "cross" && !canFlipSquare) {
+      warningTimer = 120; //2 seconds on 60fps
+    } else {
+      flipMode = "cross";
+    }
+  }
+}
 
-  //flip current tile 
-  //upgrade: only do this if mouse is on canvas
-  
+function flipWarning(){
+    if (warningTimer > 0) {
+    warningTimer--;
+    fill("red");
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    text("Grid too small to use square flip!", width / 2, height - 50);
+  }
+}
+
+
+
+function mousePressed(){
+
   let x = getCurrentX();
   let y = getCurrentY();
 
   //ALWAYS: Flip the "focused" tile
-
-
-
   // IF THEY EXIST:
   // Flip our NSEW neighbours (cross pattern)
   
   // flip(x - 1, y)
   // flip(x, y + 1)
   // flip(x, y - 1)
+
+
+
   if (keyIsDown(SHIFT)){
     flip(x, y);
   }
   else{
+    if(flipMode === "cross"){
     flip(x, y);
     if(x + 1 < cols) flip(x + 1, y);
     if(y - 1 >= 0) flip(x, y - 1);
     if(x - 1 >= 0) flip(x - 1, y);
     if(y + 1 < rows) flip(x, y + 1);
+    }
+
+    else if (flipMode === "square") {
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          let nx = x + j;
+          let ny = y + i;
+          if (nx >= 0 && nx < cols && ny >= 0 && ny < rows) {
+            flip(nx, ny);
+          }
+        }
+      }
+    }
   }
 }
 
@@ -111,13 +149,17 @@ function flip(x, y){
 
 function getCurrentX(){
   //determine current col of mouse position
-  let constrainedX = constrain(mouseX, 0, (cols*squareSize) - 1)
+  let gridWidth = cols * squareSize;
+  let startX = (width - gridWidth) / 2;
+  let constrainedX = constrain(mouseX - startX, 0, gridWidth - 1);
   return floor(constrainedX / squareSize);
 }
 
 function getCurrentY(){
   //determine current row of mouse position
-  let constrainedY = constrain(mouseY, 0, (rows*squareSize) - 1)
+  let gridHeight = rows * squareSize;
+  let startY = (height - gridHeight) / 2;
+  let constrainedY = constrain(mouseY - startY, 0, gridHeight - 1);
   return floor(constrainedY / squareSize);
 }
 
@@ -128,9 +170,10 @@ function checkWin(){
       if(grid[y][x] === 0){
         count++
         if(count === maxSquares){
+          textAlign(CENTER, CENTER);
+          fill('lime');
           textSize(50);
-          fill("lime");
-          text("YOU WON", width/2, height/2);
+          text("YOU WON!", width / 2, height / 2);
         }
       }
     }
